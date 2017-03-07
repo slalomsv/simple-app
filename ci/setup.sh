74b7e5729fa451a1b7cc8fe64e3820e3539859d3
Simon So <simon.so@gmail.com>
2017-03-07 14:38:34 -0800

    debug

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -x
+
 echo ">>> Generating SSH key pair"
 ssh-keygen -t rsa -C "user0001@slalom.com" -b 4096 -f id_rsa -N ""
 
-echo ">>> Importing public key into AWS"
+echo ">>> Importing key pair into AWS"
 AWS_KEY_NAME=MyKeyPair
 aws ec2 import-key-pair --key-name "$AWS_KEY_NAME" --public-key-material "$(cat id_rsa.pub)"
 
@@ -13,14 +15,24 @@ echo ">>> EC2 instance id: $INSTANCE_ID"
 echo "$INSTANCE_ID" > ec2_instance_id
 
 echo ">>> Grabbing public ip"
+aws ec2 describe-instances | jq '.Reservations[].Instances[] | select(.InstanceId=="$INSTANCE_ID")'
 PUBLIC_IP=`aws ec2 describe-instances | jq '.Reservations[].Instances[] | select(.InstanceId=="$INSTANCE_ID") | .PublicIpAddress'`
 while [ -z $PUBLIC_IP ]; do
-    echo ">>> IP not ready. Trying again in 10"
-    sleep 10
-    aws ec2 describe-instances | jq '.Reservations[].Instances[] | select(.InstanceId=="$INSTANCE_ID") | .PublicIpAddress'
-    PUBLIC_IP=`aws ec2 describe-instances | jq '.Reservations[].Instances[] | select(.InstanceId=="$INSTANCE_ID") | .PublicIpAddress'`
+  echo ">>> Public IP not associated yet. Trying again in 10s"
+  sleep 10
+  aws ec2 describe-instances | jq '.Reservations[].Instances[] | select(.InstanceId=="$INSTANCE_ID") | .PublicIpAddress'
+  PUBLIC_IP=`aws ec2 describe-instances | jq '.Reservations[].Instances[] | select(.InstanceId=="$INSTANCE_ID") | .PublicIpAddress'`
 done    
-echo ">>> public ip: $PUBLIC_IP"
+echo ">>> Public IP: $PUBLIC_IP"
+
+STATUS=`aws ec2 describe-instances | jq '.Reservations[].Instances[] | select(.InstanceId=="$INSTANCE_ID") | .State.Name'`
+while [ $STATUS != "running" ]; do
+  echo ">>> Instance state: $STATUS"
+  sleep 10
+  STATUS=`aws ec2 describe-instances | jq '.Reservations[].Instances[] | select(.InstanceId=="$INSTANCE_ID") | .State.Name'`
+done
+echo ">>> Instance state: $STATUS"
+
 
 ssh -i id_rsa -o "StrictHostKeyChecking no" ubuntu@$PUBLIC_IP "uname -a"
 
--- ci/teardown.sh
+++ ci/teardown.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 AWS_KEY_NAME=MyKeyPair
-echo ">>> Deleting key pair"
+echo ">>> Deleting key pair $AWS_KEY_NAME"
 aws ec2 delete-key-pair --key-name "$AWS_KEY_NAME"
 
 INSTANCE_ID=`cat ec2_instance_id`
-echo ">>> EC2 instance id: $INSTANCE_ID"
+#echo ">>> Terminating EC2 instance $INSTANCE_ID"
+aws ec2 terminate-instances --instance-ids "$INSTANCE_ID"
 

3d29af0e8de2fc4e568cf6b35cfafa6a7f47cccb
Simon So <simon.so@gmail.com>
2017-03-07 11:52:38 -0800

    setup and teardown scripts

---
+++ ci/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo ">>> Generating SSH key pair"
+ssh-keygen -t rsa -C "user0001@slalom.com" -b 4096 -f id_rsa -N ""
+
+echo ">>> Importing public key into AWS"
+KEY_NAME=MyKeyPair
+aws ec2 import-key-pair --key-name "$KEY_NAME" --public-key-material "$(cat id_rsa.pub | base64)"
+
+echo ">>> Spinning up EC2 instance"
+INSTANCE_ID=`aws ec2 run-instances --image-id ami-156be775 --key-name "$KEY_NAME" --count 1 --instance-type t2.micro --security-group-ids sg-7d482705 --subnet-id subnet-ea2b1fb2 | jq -r ".Instances[0].InstanceId"`
+echo ">>> EC2 instance id: $INSTANCE_ID"
+echo "$INSTANCE_ID" > ec2_instance_id
---
+++ ci/teardown.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+INSTANCE_ID=`cat ec2_instance_id`
+echo ">>> EC2 instance id: $INSTANCE_ID"
+

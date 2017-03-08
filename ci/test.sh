8e66f036e3457f346565e0ac5e12c840bc4ca7e0
Simon So <simon.so@gmail.com>
2017-03-07 16:00:45 -0800

    turn of stricthostkeycheck

--- ci/test.sh
+++ ci/test.sh
@@ -3,7 +3,7 @@
 PUBLIC_IP=`cat ec2_public_ip`
 
 echo ">>> Starting server"
-ssh -i id_rsa ubuntu@$PUBLIC_IP 'cd ~/simple-app && pm2 start app.js'
+ssh -i id_rsa -o "StrictHostKeyChecking no" ubuntu@$PUBLIC_IP 'cd ~/simple-app && pm2 start app.js'
 
 echo ">>> Running tests"
-ssh -i id_rsa ubuntu@$PUBLIC_IP 'cd ~/simple-app && npm run test'
+ssh -i id_rsa -o "StrictHostKeyChecking no" ubuntu@$PUBLIC_IP 'cd ~/simple-app && npm run test'

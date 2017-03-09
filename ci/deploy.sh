3c3e13acd1c7906c12ad96e76ed9c5b4f11fa419
Simon So <simon.so@gmail.com>
2017-03-09 13:19:04 -0800

    debug transfer issues

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -13,17 +13,17 @@ fi
 
 echo ">>> Transfering package to EC2 instance"
 scp "../$PACKAGE_NAME" ubuntu@$PUBLIC_IP:~
-ssh ubuntu@$PUBLIC_IP 'ls -lart'
-
 
 if [ $? != 0 ]; then
   echo "Transfer failed. Exiting"
   exit 1
 fi
 
-#echo ">>> Remove and recreate simple-app directory"
-#ssh ubuntu@$PUBLIC_IP 'rm -rf simple-app && mkdir simple-app'
+ssh ubuntu@$PUBLIC_IP 'ls -lart | grep "$PACKAGE_NAME"'
 
-#echo ">>> Extract package contents"
-#ssh ubuntu@$PUBLIC_IP 'tar -xvjf "$PACKAGE_NAME" -C simple-app'
+echo ">>> Remove and recreate simple-app directory"
+ssh ubuntu@$PUBLIC_IP 'rm -rf simple-app && mkdir simple-app'
+
+echo ">>> Extract package contents"
+ssh ubuntu@$PUBLIC_IP 'tar -xvjf "$PACKAGE_NAME" -C simple-app'
 

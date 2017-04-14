35f49093d5e0618afb7ef0e77b5628a11b52c513
Simon So <simon.so@gmail.com>
2017-04-14 11:53:11 -0700

    sphinx of black quartz judge my vow

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var app = express();
 var dbconn = mysql.createConnection({
   "host": "testdb.cuicpsblup3n.us-west-2.rds.amazonaws.com",
   "user": "admin",
-  "password": "slalom123"
+  "password": "password123"
 });
 
-dbconn.query("USE testdb");
+dbconn.query("USE testdb")
 
-app.set("port", 3000);
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.set("port", 3000)
+app.set("view engine", "pug")
+app.set("views", path.join(__dirname, "views"))
 
 app.use(express.static(path.join(__dirname, "public")));
 
